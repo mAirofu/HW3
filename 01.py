72480a7fda9c34e1fd7deb584d524ab272b3e817
mAirofu <valeriahavrylchyk@gmail.com>
2025-03-22 15:50:15 +0200

    01.py

---
+++ 01.py
@@ -0,0 +1,29 @@
+class Animals():
+    age = 3
+    classification = "none"
+    cbd = "unknown"
+class Cat(Animals):
+    classification = "Mammalia"
+    cbd = "Yes"
+    def __init__(self):
+        print(self.age)
+        print(self.classification)
+        print(self.cbd)
+class FlyingSquirrel(Animals):
+    age = "2"
+    classification = "Mammalia"
+    cbd = "Yes"
+    def __init__(self):
+        print(self.age)
+        print(self.classification)
+        print(self.cbd)
+class AlbinoSnake(Animals):
+    classification = "Reptilia"
+    cbd = "No"
+    def __init__(self):
+        print(self.age)
+        print(self.classification)
+        print(self.cbd)
+cat = Cat()
+fs = FlyingSquirrel()
+als = AlbinoSnake()
